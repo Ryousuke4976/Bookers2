4613494e27b14677bbb66770037dc7747f48bbcd
Ryousuke4976 <satou.ryousuke0503@gmail.com>
2019-09-25 14:59:39 +0900

    [Add] 投稿編集機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,10 +22,13 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find()
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(params[:id])
   end
 
   def destroy
