2827c6177d6bb7b02f7626ec64fdb44a240ab723
Ryousuke4976 <satou.ryousuke0503@gmail.com>
2019-09-25 17:33:32 +0900

    [Modify] 不具合の修正と画像の修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '24e5d5ca2a7ea438714dfb50c8b93e8d43541a4faf5ad3f78c65b54cd31f5a38e7198ce80c8abb57d2b1f62be4d24016e976ac464c1efab54d9cec578d573797'
